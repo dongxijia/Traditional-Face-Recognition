6198f2fa1e633a5ab93e1b290ff4c94ea606fd16
dongxijia <32010416+dongxijia@users.noreply.github.com>
2019-09-23 14:41:03 +0800

    Add files via upload

--- MyHogSVM.py
+++ MyHogSVM.py
@@ -2,7 +2,8 @@ from BaseFace import BaseFace
 
 import cv2
 import numpy as np
-import sklearn
+from sklearn import svm
+import time
 #sklearn is the module of Hands-on Machine learning with scikit-learn and tensorflow
 
 import os
@@ -12,6 +13,14 @@ class MyHogSVM(BaseFace):
     "Face Recognition for face using HOG+SVM"
     def __init__(self, name_dataset, dir_data, size_img, N_identity, N_training_img):
         super(MyHogSVM, self).__init__(name_dataset, dir_data, size_img, N_identity, N_training_img)
+
+
+    def extract_HOG_img(self, img):
+        HOG_cells = self.init_cell_img(img)
+        HOG_blocks = self.combine_cells_to_block(HOG_cells)
+
+        return HOG_blocks
+
 
 
     def init_cell_img(self, img, width_size=8, binsize=9):
@@ -48,13 +57,15 @@ class MyHogSVM(BaseFace):
             for j in range(N_blocks_width):
                 #collection cells in a block
                 #把4个cell的HOG特征串联成新的block特征，LBP+h其实就是把图像分成了多个block
-                HOG_block = HOG_cells[i:i+height_size, j:j+width_size].flatten.astype(np.float32)
-                #归一化特征，这一步可能有点问题, 因为特征是向量，所有后续的所有东西都是向量或矩阵
+                HOG_block = HOG_cells[i:i+height_size, j:j+width_size].flatten().astype(np.float32)
+                #L1归一化特征，这一步可能有点问题, 因为特征是向量，所有后续的所有东西都是向量或矩阵
                 #对光照和阴影获得更好的效果
-                HOG_block /= np.sqrt(np.sum(abs(HOG_block)+1e-6))#防止0的平方根
+                HOG_block /= np.sqrt(np.sum(abs(HOG_block))+1e-6)#防止0的平方根
                 HOG_blocks[i, j] = HOG_block
 
         #把blocks的HOG特征串联起来，就是整个图像的HOG特征
+        self.size_HOG_feature = HOG_blocks.size
+        #print('size of HOG for img is %d'%self.size_HOG_feature)
         return HOG_blocks.flatten()
 
 
@@ -81,7 +92,7 @@ class MyHogSVM(BaseFace):
         dy = cv2.convertScaleAbs(dy)
         dx = cv2.convertScaleAbs(dx)
         # 计算梯度大小，结合水平梯度和竖直梯度
-        value_grd = cv2.addWeighted(dx, 0.5, dy, 0.5)
+        value_grd = cv2.addWeighted(dx, 0.5, dy, 0.5, 0)
         #imshow('test_gra', value_grd)
 
         ###########collecting histogram of gradient#####################
@@ -90,21 +101,85 @@ class MyHogSVM(BaseFace):
         #180 or 360, 效果有什么不同
         step = 180 // self.binsize_cell
         seq_bins = angle // step
-        for seq_i, val in zip(seq_bins.flatten(), value_grd.flatten):
+        seq_bins = seq_bins.flatten()
+        value_grd = value_grd.flatten()
+        for seq_i, val in zip(seq_bins.flatten(), value_grd.flatten()):
             hist[seq_i] += val
         return hist
 
 
-    def cal_hog_img(self, img_np):
-        '''
+    def get_train_data(self):
+        path_img = '/home/zx/dongxijia/CASIA-100/100/9.png'
+        test_img = cv2.imread(path_img, 0)
+        self.extract_HOG_img(test_img)
+        HOG_features = np.zeros(shape=(self.N_total_training_img, self.size_HOG_feature), dtype=np.float64)
+        class_indentity = np.zeros(shape=(self.N_total_training_img), dtype=np.int32)
+        i_img = 0
+        for id_identity in range(1, len(self.ids_training)+1):
+            for id_img in self.ids_training[id_identity-1]:
+                class_indentity[i_img] = id_identity
+                img = self.imgs_training[:, i_img].reshape(self.height, -1)
+                #img = self.imgs_training[:(id_identity-1)*self.N_training_img+id_img-1].reshape(self.height, -1)
+                HOG_features[i_img] = self.extract_HOG_img(img)
+                i_img += 1
+        print('Get train data down...')
 
-        :param img_np:
-        :return: HOG of img
-        '''
-        #计算水平和竖直方向的梯度,Sobel函数求完导数后会有负值，还有会大于255的值,要用16位有符号数。
+        return HOG_features, class_indentity
 
-        #可以用opencv查看梯度value的图像
-        width_cell, height_cell = 8, 8
+
+    def train_svm(self, x_train, y_train):
+        print('Start train...')
+        self.lin_clf = svm.LinearSVC()
+        self.lin_clf.fit(x_train, y_train)
+        print('Train down...')
+
+    def pridict_identify(self, path_img):
+        img = cv2.imread(path_img,  0)
+        HOG_feature = self.extract_HOG_img(img)
+        id_result = self.lin_clf.predict([HOG_feature])
+        #print(id_result)
+        return id_result
+
+
+    def verification(self):
+        print('Evaluation start..')
+        results_file = os.path.join('results', 'HOGSVM_%s_results_t.txt' % self.name_dataset)
+
+        if not os.path.exists('results'):
+            os.makedirs('results')
+        test_cout = self.N_testing_img * self.N_identity
+        #cout_correct = 0.0
+        ture_label = np.zeros(shape=(test_cout), dtype=np.int32)
+        test_features = np.zeros(shape=(test_cout, self.size_HOG_feature), dtype=np.float32)
+
+        i_img = 0
+        with open(results_file, 'w') as f:
+            for id_face in range(1, self.N_identity+1):
+                #把没有用于训练的图像拿来做验证
+                for id_test in range(1, 11):
+                    if id_test not in self.ids_training[id_face-1]:
+                        path_to_img = os.path.join(self.dir_identity, str(id_face),str(id_test)+self.type_img)
+                        img = cv2.imread(path_to_img, 0)
+                        ture_label[i_img] = id_face
+                        HOG_feature = self.extract_HOG_img(img)
+                        test_features[i_img] = HOG_feature
+                        i_img += 1
+
+            predict_label = self.lin_clf.predict(test_features)
+            correct = (ture_label == predict_label)
+            self.accuary = np.sum(correct)*1.0/test_cout
+            print('Correct: %f'%(self.accuary))
+            f.write('Correct: %f'%(self.accuary))
+        print('Evaluation end...')
+
+
+
+
+
+
+
+
+
 
 
 
@@ -113,19 +188,39 @@ class MyHogSVM(BaseFace):
 
 
 def parse_args():
-    parser = argparse.ArgumentParser(description='My Fisherface parameters')
+    parser = argparse.ArgumentParser(description='My HOG+SVM parameters')
     # general
-    parser.add_argument('--name_dataset', default='att', help='name of dataset, including att and CASIA-500')
-    parser.add_argument('--dir_data', default='/home/zx/dongxijia/Eigenfaces/Eigenfaces/att_faces', help='training and testing set directory')
-    #parser.add_argument('--dir_data', default='/home/zx/dongxijia/data_for_test_model_performance',
-    #                    help='training and testing set directory')
-    parser.add_argument('--size_img', default='92*112', help='image size')
-    parser.add_argument('--N_identity', default=40, help='Numbers of dataset person')
-    parser.add_argument('--N_training_img', default=5, help='Number of person for training in a indentity, rest is test')
+    parser.add_argument('--name_dataset', default='CASIA-100', help='name of dataset, including att and CASIA-500')
+    #parser.add_argument('--dir_data', default='/home/zx/dongxijia/Eigenfaces/Eigenfaces/att_faces', help='training and testing set directory')
+    parser.add_argument('--dir_data', default='/home/zx/dongxijia/CASIA-100',
+                        help='training and testing set directory')
+    parser.add_argument('--size_img', default='112*112', help='image size')
+    parser.add_argument('--N_identity', default=100, help='Numbers of dataset person')
+    parser.add_argument('--N_training_img', default=6, help='Number of person for training in a indentity, rest is test')
 
     args = parser.parse_args()
+    return args
 
 
 if __name__ == '__main__':
     args = parse_args()
-    Sample_HOG = MyHogSVM(args.name_dataset, args.dir_data, args.size_img, args.N_identity, args.N_training_img)
+    accuarys = []
+    since = time.time()
+    for i in range(10):
+
+        Sample_HOG = MyHogSVM(args.name_dataset, args.dir_data, args.size_img, args.N_identity,
+                                       args.N_training_img)
+        x_train, y_train = Sample_HOG.get_train_data()
+        Sample_HOG.train_svm(x_train, y_train)
+        Sample_HOG.verification()
+        accuarys.append(Sample_HOG.accuary)
+
+    average_time = (time.time() - since) / 10
+    print('Average time: %d s' % average_time)
+    print('Number of training images %d' % args.N_training_img)
+    print('Mean accury is %.4f' % np.mean(accuarys))
+    '''
+    for i in range(1, 41):
+        id_result = Sample_HOG.pridict_identify('/home/zx/dongxijia/Eigenfaces/Eigenfaces/att_faces/%d/6.pgm'%i)
+        print('Ture id is %d, predict is %d'%(i, id_result[0]))
+    '''
